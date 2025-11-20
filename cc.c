e9da79a47a5a7afdcba64663137cd6dbd07815b5
Streamshaper <13stavros@gmail.com>
2025-11-20 11:18:33 +0200

    Broken.

--- cc.c
+++ cc.c
@@ -6,8 +6,63 @@
 #include <stdatomic.h>
 #include <unistd.h>
 #include <pthread.h>
+
 #include "cca.h"
 
+// ----------------------------------
+// GLOBAL GRAPH STORAGE
+// ----------------------------------
+int n_nodes;
+int n_elements;
+int* ind_ptr;
+int* indices;
+
+// ----------------------------------
+// RUNTIME CONTROL
+// ----------------------------------
+int n_threads;
+int n_active;
+int bench_active = 0;
+
+volatile int stop_flag = 0;
+_Atomic int work_index;
+
+// ----------------------------------
+// CUSTOM PORTABLE BARRIER
+// ----------------------------------
+typedef struct {
+    pthread_mutex_t mutex;
+    pthread_cond_t  cond;
+    int count;
+    int waiting;
+} my_barrier_t;
+
+void my_barrier_init(my_barrier_t *b, int count) {
+    pthread_mutex_init(&b->mutex, NULL);
+    pthread_cond_init(&b->cond, NULL);
+    b->count   = count;
+    b->waiting = 0;
+}
+
+void my_barrier_wait(my_barrier_t *b) {
+    pthread_mutex_lock(&b->mutex);
+    b->waiting++;
+    if (b->waiting == b->count) {
+        b->waiting = 0;
+        pthread_cond_broadcast(&b->cond);
+    } else {
+        while (pthread_cond_wait(&b->cond, &b->mutex) != 0);
+    }
+    pthread_mutex_unlock(&b->mutex);
+}
+
+// Two global barriers
+my_barrier_t start_barrier;
+my_barrier_t end_barrier;
+
+// ----------------------------------
+// THREAD ARG STRUCT
+// ----------------------------------
 typedef struct {
     int tid;
     int n_threads;
@@ -20,149 +75,169 @@ typedef struct {
     int* next_active;
 
     int iteration;
+
     int local_active_count;
+    unsigned char *local_next_active;   // per-thread buffer
 } thread_data_t;
 
 
+// ----------------------------------
+// TIMING UTILITY
+// ----------------------------------
 double wall_time() {
     struct timeval t;
     gettimeofday(&t, NULL);
     return t.tv_sec + t.tv_usec * 1e-6;
 }
 
-// Global matrix data
-int n_nodes;
-int n_elements;
-int* indices;
-int* ind_ptr;
 
-// Global control
-int n_active;
-int n_threads;
-int bench_active = 0;
+// ----------------------------------
+// FORWARD DECLARATIONS
+// ----------------------------------
+void* worker(void* arg);
+void initialize_labels(int* labels, int* active, int nodes);
+int unique_elements(int* labels);
+void print_update(int iter, int n_active);
+void open_matrix(char* name);
 
-// Pthread work-stealing infrastructure
-pthread_barrier_t start_barrier;
-pthread_barrier_t end_barrier;
 
-volatile int stop_flag = 0;
-_Atomic int work_index;
-
-int main (int argc, char* argv[])
+// ----------------------------------
+// MAIN
+// ----------------------------------
+int main(int argc, char* argv[])
 {
-    if (argc > 1 && atoi(argv[1])==1)
+    if (argc > 1 && atoi(argv[1]) == 1)
         bench_active = 1;
 
     n_threads = sysconf(_SC_NPROCESSORS_ONLN);
     if (argc > 2) {
-        if (atoi(argv[2]) <= sysconf(_SC_NPROCESSORS_ONLN))
-            n_threads = atoi(argv[2]);
+        int req = atoi(argv[2]);
+        if (req > 0 && req <= n_threads)
+            n_threads = req;
     }
 
-    open_matrix ("com-LiveJournal.mat");
+    open_matrix("com-LiveJournal.mat");
 
     double t0 = wall_time();
-    
-    int* labels = malloc (n_nodes*sizeof(int));  
-    int* active = malloc (n_nodes*sizeof(int));  
-    int* next_active = malloc (n_nodes*sizeof(int)); 
+
+    int* labels      = malloc(n_nodes * sizeof(int));
+    int* active      = malloc(n_nodes * sizeof(int));
+    int* next_active = malloc(n_nodes * sizeof(int));
+
+    memset(next_active, 0, n_nodes * sizeof(int));
 
     int iteration = 0;
     n_active = n_nodes;
 
-    initialize_labels (labels, active, n_nodes);
+    initialize_labels(labels, active, n_nodes);
 
+    // Create persistent threads
     pthread_t threads[n_threads];
     thread_data_t td[n_threads];
 
-    // Initialize the pthread barriers
-    pthread_barrier_init(&start_barrier, NULL, n_threads + 1);
-    pthread_barrier_init(&end_barrier,   NULL, n_threads + 1);
+    my_barrier_init(&start_barrier, n_threads + 1);
+    my_barrier_init(&end_barrier,   n_threads + 1);
 
-    // Create persistent workers
     for (int t = 0; t < n_threads; t++)
     {
-        td[t].tid       = t;
-        td[t].n_threads = n_threads;
-        td[t].n_nodes   = n_nodes;
+        td[t].tid           = t;
+        td[t].n_threads     = n_threads;
+        td[t].n_nodes       = n_nodes;
 
-        td[t].ind_ptr     = ind_ptr;
-        td[t].indices     = indices;
-        td[t].labels      = labels;
-        td[t].active      = active;
-        td[t].next_active = next_active;
+        td[t].ind_ptr       = ind_ptr;
+        td[t].indices       = indices;
+        td[t].labels        = labels;
+        td[t].active        = active;
+        td[t].next_active   = next_active;
 
-        td[t].iteration   = iteration;
+        td[t].iteration     = iteration;
+        td[t].local_active_count = 0;
+
+        // Allocate private buffer, memset lazily per iteration
+        td[t].local_next_active = malloc(n_nodes);
+        memset(td[t].local_next_active, 0, n_nodes);
 
         pthread_create(&threads[t], NULL, worker, &td[t]);
     }
 
+
+
+    // ----------------------------------
+    // MAIN LOOP
+    // ----------------------------------
     while (n_active > 0)
     {
         iteration++;
 
-        // Publish iteration marker to workers
         for (int t = 0; t < n_threads; t++)
-            td[t].iteration = iteration;
+        {
+            td[t].iteration     = iteration;
+            td[t].active        = active;
+            td[t].next_active   = next_active;
+            memset(td[t].local_next_active, 0, n_nodes);
+        }
 
-        // Reset global work pointer
         atomic_store(&work_index, 0);
 
-        // Wake workers to start this iteration
-        pthread_barrier_wait(&start_barrier);
+        my_barrier_wait(&start_barrier);
+        my_barrier_wait(&end_barrier);
 
-        // Wait for them to finish
-        pthread_barrier_wait(&end_barrier);
-
-        // Reduction: sum per-thread local active counts
+        // Combine thread-local activity counts
         int n_active_next = 0;
         for (int t = 0; t < n_threads; t++)
             n_active_next += td[t].local_active_count;
 
         n_active = n_active_next;
 
-        if(!bench_active)
-            print_update (iteration, n_active);
+        if (!bench_active)
+            print_update(iteration, n_active);
 
-        // Swap active arrays
-        int* temp = active;
+        // Swap active buffers
+        int* tmp = active;
         active = next_active;
-        next_active = temp;
+        next_active = tmp;
     }
 
-    // Stop workers cleanly
-    stop_flag = 1;
-    pthread_barrier_wait(&start_barrier);
-    for (int t = 0; t < n_threads; t++)
-        pthread_join(threads[t], NULL);
 
-    pthread_barrier_destroy(&start_barrier);
-    pthread_barrier_destroy(&end_barrier);
+    // ----------------------------------
+    // SHUTDOWN
+    // ----------------------------------
+    stop_flag = 1;
+    my_barrier_wait(&start_barrier);
+
+    for (int t = 0; t < n_threads; t++) {
+        pthread_join(threads[t], NULL);
+        free(td[t].local_next_active);
+    }
 
     double t1 = wall_time();
+
     if (!bench_active)
-        printf ("Total Connected Components: %d, found in %lf seconds!\n", 
-                unique_elements(labels), t1-t0);
+        printf("Total Connected Components: %d, found in %lf seconds!\n",
+               unique_elements(labels), t1 - t0);
     else
-        printf ("%lf", t1-t0);
+        printf("%lf", t1 - t0);
 
     free(ind_ptr);
     free(indices);
+    free(labels);
     free(active);
     free(next_active);
-    free(labels);
-    
+
     return 0;
 }
 
+
+// ----------------------------------
+// WORKER THREAD
+// ----------------------------------
 void* worker(void* arg)
 {
     thread_data_t* td = (thread_data_t*)arg;
 
     while (1)
     {
-        // Wait for main to start new iteration
-        pthread_barrier_wait(&start_barrier);
+        my_barrier_wait(&start_barrier);
 
         if (stop_flag)
             break;
@@ -171,7 +246,9 @@ void* worker(void* arg)
         int iteration = td->iteration;
         td->local_active_count = 0;
 
-        // Work stealing: threads grab node indices dynamically
+        unsigned char *local = td->local_next_active;
+
+        // Parallel for using atomic index
         int i = atomic_fetch_add(&work_index, 1);
 
         while (i < n_nodes)
@@ -180,15 +257,16 @@ void* worker(void* arg)
             {
                 int s = td->ind_ptr[i];
                 int e = td->ind_ptr[i+1];
+
                 if (s != e)
                 {
                     int min_label = td->labels[ td->indices[s] ];
 
                     for (int k = s + 1; k < e; k++)
                     {
-                        int l = td->labels[ td->indices[k] ];
-                        if (l < min_label)
-                            min_label = l;
+                        int lbl = td->labels[td->indices[k]];
+                        if (lbl < min_label)
+                            min_label = lbl;
                     }
 
                     if (min_label < td->labels[i])
@@ -198,10 +276,9 @@ void* worker(void* arg)
                         for (int k = s; k < e; k++)
                         {
                             int nb = td->indices[k];
-
-                            if (td->next_active[nb] != iteration + 1)
+                            if (!local[nb])
                             {
-                                td->next_active[nb] = iteration + 1;
+                                local[nb] = 1;
                                 td->local_active_count++;
                             }
                         }
@@ -209,88 +286,105 @@ void* worker(void* arg)
                 }
             }
 
-            // Grab another unit of work
             i = atomic_fetch_add(&work_index, 1);
         }
 
-        // Signal that this worker is finished
-        pthread_barrier_wait(&end_barrier);
+        // Merge into shared next_active buffer
+        for (int j = 0; j < n_nodes; j++)
+            if (local[j])
+                td->next_active[j] = iteration + 1;
+
+        my_barrier_wait(&end_barrier);
     }
 
     return NULL;
 }
 
-void initialize_labels (int* labels,int* active, int nodes)
+
+// ----------------------------------
+// INITIALIZATION
+// ----------------------------------
+void initialize_labels(int* labels, int* active, int nodes)
 {
-    for (int i=0; i<nodes; i++)
-    {    
-        labels[i] = i+1;
+    for (int i = 0; i < nodes; i++)
+    {
+        labels[i] = i + 1;
         active[i] = 1;
     }
 }
 
-int unique_elements (int* labels)
+
+// ----------------------------------
+// COUNT UNIQUE LABELS
+// ----------------------------------
+int unique_elements(int* labels)
 {
     int sum = 0;
-    int *temp = malloc (n_nodes*sizeof(int));
+    int *temp = malloc(n_nodes * sizeof(int));
     int found = 0;
 
     temp[0] = labels[0];
     sum++;
 
-    for (int k=1; k<n_nodes; k++)
-    {    
+    for (int k = 1; k < n_nodes; k++)
+    {
         found = 0;
-        for (int l=0; l<sum; l++)
-            if (temp[l] == labels[k])
-            {
+        for (int l = 0; l < sum; l++)
+            if (temp[l] == labels[k]) {
                 found = 1;
-                l=sum;
+                break;
             }
         if (!found)
-        {
-            temp[sum] = labels[k];
-            sum++;
-        }
+            temp[sum++] = labels[k];
     }
-    free (temp);
+
+    free(temp);
     return sum;
-            
 }
 
-void print_update (int iter, int n_active)
+
+// ----------------------------------
+// PRINT ITERATION STATUS
+// ----------------------------------
+void print_update(int iter, int n_active)
 {
     printf("Iteration: %d || Still Active: %d nodes.\n", iter, n_active);
 }
 
-void open_matrix (char* name)
+
+// ----------------------------------
+// LOAD MATRIX MARKET FILE
+// ----------------------------------
+void open_matrix(char* name)
 {
     mat_t *matfp = Mat_Open(name, MAT_ACC_RDONLY);
     if (!matfp) { fprintf(stderr,"Cannot open file\n"); exit(2); }
 
     matvar_t *problem = Mat_VarRead(matfp, "Problem");
-    if (!problem || problem->class_type != MAT_C_STRUCT) { fprintf(stderr,"Problem struct missing\n"); exit(2); }
+    if (!problem || problem->class_type != MAT_C_STRUCT)
+        { fprintf(stderr,"Problem struct missing\n"); exit(2); }
 
     matvar_t *Avar = Mat_VarGetStructFieldByName(problem, "A", 0);
-    if (!Avar || Avar->class_type != MAT_C_SPARSE) { fprintf(stderr,"A is not sparse\n"); exit(2); }
+    if (!Avar || Avar->class_type != MAT_C_SPARSE)
+        { fprintf(stderr,"A is not sparse\n"); exit(2); }
 
     mat_sparse_t *A = (mat_sparse_t*)Avar->data;
     size_t m = Avar->dims[0], n = Avar->dims[1], nnz = A->nzmax;
 
-    indices = malloc (nnz*sizeof(int));
-    ind_ptr = malloc (n*sizeof(int));
+    indices = malloc(nnz * sizeof(int));
+    ind_ptr = malloc((n + 1) * sizeof(int));
 
-    for (size_t q=0; q<nnz; q++)
+    for (size_t q = 0; q < nnz; q++)
         indices[q] = (int)A->ir[q];
 
-    for (size_t q=0; q<n; q++)
+    for (size_t q = 0; q < n + 1; q++)
         ind_ptr[q] = (int)A->jc[q];
 
-    n_nodes = n;
-    n_elements = nnz/2;
+    n_nodes    = n;
+    n_elements = nnz / 2;
 
     Mat_Close(matfp);
 
     if (!bench_active)
-        printf ("Loaded matrix with %d nodes and %d elements.\n", n_nodes, n_elements);
+        printf("Loaded matrix with %d nodes and %d elements.\n", n_nodes, n_elements);
 }
